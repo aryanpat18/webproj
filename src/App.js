fa8607a5110cc8e6e5f755f8a5b4ff68c7557130
Aryan <aryanpat18@gmail.com>
2024-01-01 15:14:05 -0700

    git push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import Header from "./components/Header";
-import Home from "./components/Home.jsx";
-import About from "./components/About.jsx";
-import Project from "./components/Projects.jsx";
-import Contact from "./components/Contact.jsx";
-import Skills from "./components/Skills.jsx";
-import Experience from "./components/Experience.jsx";
-import Footer from "./components/Footer.jsx";
-
+import Home from "./components/Home";
+import About from "./components/About";
+import Project from "./components/Projects";
+import Contact from "./components/Contact";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-    <Header/>
-    <Home/>
-    <About/>
-    <Project/>
-    <Experience/>
-    <Skills/>
-    <Contact/>
-    <Footer/>
+      <Header />
+      <Home />
+      <About />
+      <Project />
+      <Experience />
+      <Skills />
+      <Contact />
+      <Footer />
     </>
   );
 }
